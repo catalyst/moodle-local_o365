f4b855e301a7938bd41329818e51fc651157ad0a
Chen Li <110831315+chenli12138@users.noreply.github.com>
2025-03-31 16:29:55 +1100

    Fix external_format_value not found issue (#3)
    * Fix external_format_value not found issue
    
    * import external_format_value
    
    * chaning import order
    
    ---------
    
    Co-authored-by: chenli <chenli@your.hostname.com>

--- classes/webservices/read_assignments.php
+++ classes/webservices/read_assignments.php
@@ -28,7 +28,9 @@ namespace local_o365\webservices;
 use assign;
 use context_course;
 use context_module;
+
 use external_api;
+use external_format_value;
 use external_function_parameters;
 use external_multiple_structure;
 use external_single_structure;
